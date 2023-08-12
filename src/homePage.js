4ddb12d47ffc8034d2b60e089382cb76f200e692
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-08-11 22:03:50 -0400

    Create homePage

--- src/homePage.js
+++ src/homePage.js
@@ -1,2 +1,22 @@
-import './homePageStle.css';
+import './style.css';
+import mainPicImage from './muscleMan.png';
 
+export default function displayHomePage(){
+    const content = document.querySelector('#content');
+    const homeContainer = document.createElement('div');
+    homeContainer.classList.add('home-page-container');
+
+    const restaurantInfo = document.createElement('p');
+    restaurantInfo.textContent = 'Fuel your gains with our protein packed meals.'
+    homeContainer.appendChild(restaurantInfo);
+
+    const mainPic = new Image();
+    mainPic.src = mainPicImage;
+    homeContainer.appendChild(mainPic);
+
+    const closingMessage = document.createElement('p');
+    closingMessage.textContent = 'Your gains are waiting, stop by now!';
+    homeContainer.appendChild(closingMessage);
+
+    content.appendChild(homeContainer);
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import './style.css';
 import displayNavbar from "./navbar";
+import displayHomePage from './homePage';
+import githubIcon from './logo-github.svg';
 
 function displayRestaurantName() {
     const content = document.querySelector('#content');
     const header = document.createElement('h1');
 
-    header.textContent = 'The Ultimate Protein Restaurant';
+    header.textContent = 'GainStop';
     header.classList.add('header');
     content.appendChild(header);
 }
 
-displayRestaurantName();
-displayNavbar();
+function displayHeader() {
+    displayRestaurantName();
+    displayNavbar();
+}
+
+function displayFooter() {
+    const content = document.querySelector('#content');
+    const footer = document.createElement('div');
+    footer.classList.add('footer');
+    
+    const copyrightSpan = document.createElement('span');
+    copyrightSpan.textContent = 'Copyright @ 2023 DeveloperDave17';
+    footer.appendChild(copyrightSpan);
+
+    const githubImage = new Image();
+    githubImage.src = githubIcon;
+    footer.appendChild(githubImage);
+
+    content.appendChild(footer);
+}
+
+displayHeader();
 const navBarContainer = document.querySelector('.navbar-container');
 (navBarContainer.childNodes)[0].classList.add('selected');
+displayHomePage();
+displayFooter();
